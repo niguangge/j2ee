6b8caebd4d7bdbdde6028b5e2d0a04e1990b8cf0
wenjun <wen292006955@qq.com>
2017-07-28 17:25:43 +0800

    11

---
+++ j2ee/src/bean/Hero.java
@@ -0,0 +1,36 @@
+package bean;
+
+public class Hero {
+	private int id;
+	private String name;
+	private float hp;
+	private int damage;
+	public Hero() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getHp() {
+		return hp;
+	}
+	public void setHp(float hp) {
+		this.hp = hp;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+}
---
+++ j2ee/src/servlet/HeroAddServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+ 
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import bean.Hero;
+import dao.HeroDao;
+ 
+public class HeroAddServlet extends HttpServlet {
+ 
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        request.setCharacterEncoding("UTF-8");
+ 
+        Hero hero = new Hero();
+        hero.setName(request.getParameter("name"));
+        hero.setHp(Float.parseFloat(request.getParameter("hp")));
+        hero.setDamage(Integer.parseInt(request.getParameter("damage")));
+         
+        new HeroDao().add(hero);
+ 
+        response.sendRedirect("/j2ee/listHero");
+         
+    }
+}
